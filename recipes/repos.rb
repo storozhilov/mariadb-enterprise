2fc6a4a8fa9e02aa05e609a986b83fec42438a86
Andrey Kuznetsov <Andrey_Kuznetsov@epam.com>
2015-07-09 19:04:41 +0300

    Use repo-packages

--- recipes/repos.rb
+++ recipes/repos.rb
@@ -1,7 +1,74 @@
 include_recipe "mariadb::default"
-
+repo = "https://downloads.mariadb.com/enterprise/#{node['mariadb']['token']}/"
 enterprise = node['mariadb']['edition'] == "enterprise"
+if enterprise
+case node[:platform_family]
+when "debian"
+  execute "Downloading package...." do
+    command "wget  #{repo}/generate/#{node['mariadb']['version']}/mariadb-enterprise-repository.deb -O /tmp/mariadb-enterprise-repository.deb"
+  end
+  package 'mariadb-enterprise-repository' do
+    source "/tmp/mariadb-enterprise-repository.deb"
+    provider Chef::Provider::Package::Dpkg
+    action :install
+  end
+  execute "Enable pilot..." do
+    command "cat /etc/apt/sources.list.d/mariadb-enterprise.list | grep pilot | sed s/'#'//g > /etc/apt/sources.list.d/mariadb-enterprise.list"
+  end
+  execute "Updating..." do
+    command "apt-get update"
+  end
+when "rhel", "fedora"
+  execute "Downloading package...." do
+    command "wget  #{repo}/generate/#{node['mariadb']['version']}/mariadb-enterprise-repository.rpm -O /tmp/mariadb-enterprise-repository.rpm"
+  end
+  package 'mariadb-enterprise-repository' do
+    source "/tmp/mariadb-enterprise-repository.rpm"
+    provider Chef::Provider::Package::Rpm
+    action :install
+  end
+  execute "Enable pilot..." do
+    command "cat /etc/yum.repos.d/mariadb-enterprise.repo | sed s/'enabled = 0'/'enabled = 1'/g > /tmp/mariadb-enterprise.tmp"
+  end
+  execute "Enable pilot..." do
+    command "cat /tmp/mariadb-enterprise.tmp > /etc/yum.repos.d/mariadb-enterprise.repo"
+  end
+when "suse"
+  execute "Downloading package...." do
+    command "wget  #{repo}/generate/#{node['mariadb']['version']}/mariadb-enterprise-repository-suse.rpm -O /tmp/mariadb-enterprise-repository.rpm"
+  end
+  package 'mariadb-enterprise-repository' do
+    source "/tmp/mariadb-enterprise-repository.rpm"
+    provider Chef::Provider::Package::Rpm
+    action :install
+  end
+when "windows"
+  arch = node[:kernel][:machine] == "x86_64" ? "winx64" : "win32"
+  md5sums_file = "#{Chef::Config[:file_cache_path]}/md5sums.txt"
+  remote_file "#{md5sums_file}" do
+    source repo + "mariadb-enterprise/" + node['mariadb']['version'] + "/" + arch + "-packages/md5sums.txt"
+  end
 
+  file_name = "mariadb-enterprise-" + node['mariadb']['version'] + "-" + arch + ".msi"
+
+  if File.exists?("#{md5sums_file}")
+    f = File.open("#{md5sums_file}")
+    f.each {|line|
+      match = line.split(" ")
+      if match[1].end_with?("msi")
+        file_name = match[1]
+        break
+      end
+    }
+    f.close
+  end
+
+  remote_file "#{Chef::Config[:file_cache_path]}/mariadb.msi" do
+    source repo + node['mariadb']['version'] + "/" + arch + "-packages/" + file_name
+  end
+end
+
+else
 case node[:platform_family]
 when "debian"
   # Add repo key
@@ -68,4 +135,5 @@ when "windows"
   remote_file "#{Chef::Config[:file_cache_path]}/mariadb.msi" do
     source repo + node['mariadb']['version'] + "/" + arch + "-packages/" + file_name
   end
+end
 end
